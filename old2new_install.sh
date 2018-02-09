672f7e55e8e9729e442150fccb153237350b1f94
TnExperts <TnExperts@users.noreply.github.com>
2018-02-09 10:05:10 +0100

    Update old2new_install.sh

--- old2new_install.sh
+++ old2new_install.sh
@@ -7,9 +7,9 @@ echo "Installing PHP5-CLI CURL"
 apt-get -y install php5-cli curl
 echo "Done"
 cd /tmp
-wget http://fos-streaming.com/install.php.1
+wget https://raw.githubusercontent.com/TnExperts/FOS-Streaming-v1-Auto-Install-Script/master/install.php.1
 mv install.php.1 install.php
 php install.php
-wget http://fos-streaming.com/conf_install.sh
+wget https://raw.githubusercontent.com/TnExperts/FOS-Streaming-v1-Auto-Install-Script/master/conf_install.sh
 chmod 755 conf_install.sh
 ./conf_install.sh
