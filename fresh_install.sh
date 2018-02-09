dfe41a48f992da23dd831869eeee36ab8ad018b6
TnExperts <TnExperts@users.noreply.github.com>
2018-02-09 10:01:03 +0100

    Update fresh_install.sh

--- fresh_install.sh
+++ fresh_install.sh
@@ -5,9 +5,9 @@ echo "Installing PHP5-CLI CURL"
 apt-get -y install php5-cli curl
 echo "Done"
 cd /tmp
-wget http://fos-streaming.com/install.php.1
+wget https://raw.githubusercontent.com/TnExperts/FOS-Streaming-v1-Auto-Install-Script/master/install.php.1
 mv install.php.1 install.php
 php install.php
-wget http://fos-streaming.com/db_install.sh
+wget https://raw.githubusercontent.com/TnExperts/FOS-Streaming-v1-Auto-Install-Script/master/db_install.sh
 chmod 755 db_install.sh
 ./db_install.sh
